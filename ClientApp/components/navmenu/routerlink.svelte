7a48f57d0f33fc76ddddc5d8b8fb93137a3b0b4c
Kiho <kihoc22@gmail.com>
2019-06-17 22:15:42 -0700

    Code cleanup

--- ClientApp/components/navmenu/routerlink.svelte
+++ ClientApp/components/navmenu/routerlink.svelte
@@ -4,46 +4,40 @@
 
     const dispatch = createEventDispatcher();
 
-    import roadtrip from "roadtrip";
+    import roadtrip from 'roadtrip';
 
-    const activedClassName = "active";
+    const activedClassName = 'active';
 
-    export let active = "";
-    export let to = "/";
-    export let location = "";
+    export let active = '';
+    export let to = '/';
+    export let location = '';
     export let item = null;
 
-    // [svelte-upgrade warning]
-    // beforeUpdate and afterUpdate handlers behave
-    // differently to their v2 counterparts
     afterUpdate(() => {
         setActivedClass(location);
     });
 
-    // [svelte-upgrade suggestion]
-    // review these functions and remove unnecessary 'export' keywords
-    export function navigate($e, to) {
-        if ($e && $e.preventDefault) $e.preventDefault();
+    function navigate(e, to) {
+        if (e && e.preventDefault) e.preventDefault();
         // console.log('roadtrip.RouteData', roadtrip.RouteData);
         if (item) {
-        dispatch("itemClick", event, item);
+            dispatch('itemClick', event, item);
         } else if (to) {
             roadtrip.goto(basePath + to);
         }
     }
 
-    export function setActivedClass(location) {
+    function setActivedClass(location) {
         if (location === `${basePath}${to}`) {
             active = activedClassName;
         } else {
-            active = "";
+            active = '';
         }
-        // console.log("setActivedClass (location)", location, `${basePath}${to}`);
     }
 </script>
 
 <li class="nav-item px-3">
-  <a class="nav-link {active}" href='{basePath}{ to }' on:click='{event => navigate(event, to)}'>
+  <a class="nav-link {active}" href="{basePath}{ to }" on:click="{e => navigate(e, to)}">
     <slot></slot>
   </a>
 </li>
